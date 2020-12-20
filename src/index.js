8050780c0761a2b47b18a3db1f75e674d6bcbc44
Williams Blanco <williamsblanco216@gmail.com>
2020-12-20 12:29:57 -0400

    firts commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import ReactDom from 'react-dom'
+import App from './components/App'
+import React from 'react';
+ReactDom.render(
+  <App />,
+  document.getElementById('root')
+)
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+
+const babelRule = {
+  test:/\.js$/i,
+  exclude: path.resolve(__dirname,'node_modules'),
+  use:{
+    loader: 'babel-loader',
+    options:{
+      presets:['@babel/preset-env', '@babel/preset-react']
+    }
+  }
+}
+
+const cssRule = {
+  test: /\.css$/i,
+  use: ['css-loader', 'style-loader']
+}
+
+const fileRule = {
+  test: /\.(png|jpe?g|gif|svg)$/i,
+  use: 'file-loader'
+}
+
+module.exports = {
+  entry: [
+    './src/index.js',
+    '@babel/polyfill'
+  ],
+  output: {
+    filename: 'index.js',
+    path: `${__dirname}/public`
+  },
+  watch: true,
+  devServer:{
+    open: true
+  },
+  plugins:[new HTMLWebpackPlugin({template: './src/index.html'})],
+  module:{
+    rules:[
+      babelRule,
+      cssRule,
+      fileRule
+    ]
+  }
+
+}
